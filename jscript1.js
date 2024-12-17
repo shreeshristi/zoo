30005e6e57123b5e8b77dd2c8e765bf35b832602
shreeshristi <shristishree321@gmail.com>
2024-12-17 14:48:47 +0530

    datatypes

--- jscript1.js
+++ jscript1.js
@@ -1,1 +1,16 @@
 console.log("shristi")
+const accountId=2023048
+let accountemail= "electrical048@gmail.com"
+var accountpassword="12345"
+accountcity="Jaipur"
+// accountId= 202200255
+
+console.log(accountId);
+let accountstate;
+console.table([accountId, accountemail, accountpassword,accountcity,accountstate]);
+
+//datatypes
+console.log(typeof null);
+console.log(typeof undefined);
+
+
