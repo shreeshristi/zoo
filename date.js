f61e80507d60f45ee6d020ce0efec54b159ab998
shreeshristi <shristishree321@gmail.com>
2024-12-19 11:43:18 +0530

    ardate

---
+++ date.js
@@ -0,0 +1,8 @@
+let mydate= new Date();
+console.log(mydate)
+mydate.toLocaleString({
+    weekday: "long"
+    
+
+})
+console.log(mydate)
--- jscript1.js
+++ jscript1.js
@@ -1,17 +1,25 @@
-console.log("shristi")
-const accountId=2023048
-let accountemail= "electrical048@gmail.com"
-var accountpassword="12345"
-accountcity="Jaipur"
-// accountId= 202200255
+// console.log("shristi")
+// const accountId=2023048
+// let accountemail= "electrical048@gmail.com"
+// var accountpassword="12345"
+// accountcity="Jaipur"
+// // accountId= 202200255
 
-console.log(accountId);
-let accountstate;
-console.table([accountId, accountemail, accountpassword,accountcity,accountstate]);
+// console.log(accountId);
+// let accountstate;
+// console.table([accountId, accountemail, accountpassword,accountcity,accountstate]);
 
-//datatypes
-console.log(typeof null);
-console.log(typeof undefined);
+// //datatypes
+// console.log(typeof null);
+// console.log(typeof undefined);
 
 
+// const name="snehaduta"
+// const str= name.slice(-9,4)
 
+// console.log(str);
+const num=1234;
+console.log(num);
+console.log(num.toFixed(2));
+const hund=1458;
+console.log(hund.toPrecision(2));
