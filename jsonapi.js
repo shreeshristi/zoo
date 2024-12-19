2711757104e02e189356fd99b9b97bdc6fa54934
shreeshristi <shristishree321@gmail.com>
2024-12-19 13:21:15 +0530

    arr jsonapi

---
+++ jsonapi.js
@@ -0,0 +1,6 @@
+// {
+//     "name":"shristi",
+//     "age":"24"
+//     "price":"2434"
+// }
+{"results":[{"gender":"male","name":{"title":"Mr","first":"Allan","last":"Sims"},"location":{"street":{"number":2907,"name":"Galway Road"},"city":"Thurles","state":"Galway City","country":"Ireland","postcode":93800,"coordinates":{"latitude":"3.7970","longitude":"-47.0926"},"timezone":{"offset":"+4:30","description":"Kabul"}},"email":"allan.sims@example.com","login":{"uuid":"0da42e5e-3f4b-4bcc-86bf-61c3f7799a4b","username":"sadzebra439","password":"havana","salt":"o0rSTVYs","md5":"dcf91b0fdfcd21358f9446f1a2d12f9a","sha1":"78c932e65163df58ecce3ce8c429866cdd6ff2d1","sha256":"c21cebdea042e4159dacddd631ea1ebcdd8fd0e199eddbf65a4ab91e31e13faf"},"dob":{"date":"1951-01-11T05:36:29.503Z","age":73},"registered":{"date":"2018-05-18T03:04:29.927Z","age":6},"phone":"071-445-6627","cell":"081-764-4703","id":{"name":"PPS","value":"9430967T"},"picture":{"large":"https://randomuser.me/api/portraits/men/21.jpg","medium":"https://randomuser.me/api/portraits/med/men/21.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/21.jpg"},"nat":"IE"}],"info":{"seed":"e404496b715f2366","results":1,"page":1,"version":"1.4"}}
---
+++ objects.js
@@ -0,0 +1,18 @@
+// // const mysym= symbol("key1")
+// const user={
+//     name : "shristi",
+//     "full name":"shristi shree",
+//     age: 18
+//     mysym: 'keyyy'
+
+// }
+// console.log(user[mysym])
+
+const course={
+    name: "jspro",
+    price:"699",
+    instructor:"hitesh"
+}
+//de structure of object
+const { instructor: ins}= course
+console.log(ins);
